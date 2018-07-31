f86bf8b7e5b0827e2a0466199308a3babdc41db1
Stephen Reyes <sjreyes1@yahoo.com>
2018-07-31 13:20:04 -0700

    fetching a list of products.

--- daily-dose/Services/PurchaseManager.swift
+++ daily-dose/Services/PurchaseManager.swift
@@ -11,6 +11,30 @@ import StoreKit
 
 class PurchaseManager: NSObject, SKProductsRequestDelegate, SKPaymentTransactionObserver {
     
+    let IAP_REMOVE_ADS = "com.email.daily.dose.remove.ads"
+    
     static let instance  = purchaseManager()
+    
+    var productsRequest: SKProductsRequest!
+    var products = [SKProduct]()
+    
+    // the itunes store will not be able to test your products if there is nothing to fetch them with... thus the function that follows exmplifies what is needed to work with a simple product request.
+    func fetchProducts(){
+        let productIDs = NSSet(object: IAP_REMOVE_ADS) as! Set<String>
+        productsRequest = SKProductsRequest(productIdentifiers: productIDs)
+        productsRequest.delegate = self
+        productsRequest.start()
+    }
+    
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        if response.products.count > 0 {
+            print(response.products.debugDescription)
+            products = response.products
+        }
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, shouldAddStorePayment payment: SKPayment, for product: SKProduct) -> Bool {
+        <#code#>
+    }
     
 }
