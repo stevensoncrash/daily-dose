c6fc19aa4c20c21a62200ade7c89314d14e23c8d
Stephen Reyes <sjreyes1@yahoo.com>
2018-07-31 13:42:59 -0700

    recommit of in app purchase.

--- daily-dose/Services/PurchaseManager.swift
+++ daily-dose/Services/PurchaseManager.swift
@@ -19,11 +19,23 @@ class PurchaseManager: NSObject, SKProductsRequestDelegate, SKPaymentTransaction
     var products = [SKProduct]()
     
     // the itunes store will not be able to test your products if there is nothing to fetch them with... thus the function that follows exmplifies what is needed to work with a simple product request.
+    
     func fetchProducts(){
         let productIDs = NSSet(object: IAP_REMOVE_ADS) as! Set<String>
         productsRequest = SKProductsRequest(productIdentifiers: productIDs)
         productsRequest.delegate = self
         productsRequest.start()
+    }
+    
+    func purchaseRemoveAds(){
+        
+        if SKPaymentQueue.canMakePayments() && products.count > 0 {
+            let removeAdsProduct = products[0]
+            let payment = SKPayment(product: removeAdsProduct)
+            SKPaymentQueue.default().add(self)
+            SKPaymentQueue.default().add(payment)
+        }
+        
     }
     
     func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
@@ -33,8 +45,24 @@ class PurchaseManager: NSObject, SKProductsRequestDelegate, SKPaymentTransaction
         }
     }
     
-    func paymentQueue(_ queue: SKPaymentQueue, shouldAddStorePayment payment: SKPayment, for product: SKProduct) -> Bool {
-        <#code#>
+    func products func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transation in transactions {
+            switch transation.transactionState{
+            case.purchased:
+            SKPaymentQueue.default().finishTransaction(transation)
+            if transation.payment.productIdentifier == IAP_REMOVE_ADS {
+                UserDefaults.standard.set(true, forKey: IAP_REMOVE_ADS)
+            }
+                    break
+            case.failed:
+                SKPaymentQueue.default().finishTransaction(transation)
+                    break
+            case.restored:
+                SKPaymentQueue.default().finishTransaction(transation)
+            default: break
+                
+            }
+        }
     }
     
 }
